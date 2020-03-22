59b805c2f6e286ec7b2f592fad50d0b62e3022aa
Praveen Bisht <prvnbist@gmail.com>
2020-03-22 10:48:13 +0530

    refactor: group by entity

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,19 @@
-const Recipe = require('./recipe')
-const PurchaseOrder = require('./purchase_order')
-const SupplierItemProcessingSachet = require('./supplier_item_processing_sachets')
-const SupplierItemProcessing = require('./supplier_item_processing')
-const SupplierItem = require('./supplier_item')
-const Supplier = require('./supplier')
-const WorkOrder = require('./work_order')
+import { Recipe } from './recipe'
+const {
+   Supplier,
+   WorkOrder,
+   SupplierItem,
+   PurchaseOrder,
+   SupplierItemProcessing,
+   SupplierItemProcessingSachet
+} = require('./inventory/purchase_order')
 
-module.exports = { Recipe, PurchaseOrder, SupplierItemProcessingSachet, SupplierItemProcessing, Supplier, SupplierItem, WorkOrder }
+module.exports = {
+   Recipe,
+   Supplier,
+   WorkOrder,
+   SupplierItem,
+   PurchaseOrder,
+   SupplierItemProcessing,
+   SupplierItemProcessingSachet
+}
